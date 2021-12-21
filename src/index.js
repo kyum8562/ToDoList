3d54b8638bd9d76853fd75f2ac41f42e4296f791
kyum8562@gmail.com <66922359+kyum8562@users.noreply.github.com>
2021-12-21 11:53:06 +0900

    1221-1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // APp.js를 가리킴
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
